1da9430f0f011061ffb4c1dfbb5995403ebca56d
thekingvice <villarrealvicente92@gmail.com>
2023-07-23 23:19:04 -0700

    commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="ScoreWrapper">
+    <img class="Logo" src="public/images/logo-bonus.svg" alt="logo" />
+    <div class="ScoreCard">
+      <div class="ScoreCard__Text">SCORE</div>
+      <div class="Score">01</div>
+    </div>
+  </section>
+  <button class="PlayButton">PLAY AGAIN</button>
+</template>
+
+<style scoped>
+.ScoreWrapper {
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+  width: 20rem;
+  border: 3px solid hsl(217, 16%, 45%);
+  border-radius: 0.5rem;
+  padding: 1rem;
+}
+.ScoreCard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem 2rem;
+  border-radius: 0.5rem;
+  background: var(--white);
+}
+.ScoreCard__Text {
+  color: var(--score-text);
+}
+
+.Score {
+  font-size: 2rem;
+}
+.Logo {
+  width: 7rem;
+}
+
+.PlayButton {
+  padding: 0.5rem 3rem;
+  border-radius: 0.25rem;
+  background: var(--white);
+  color: var(--dark-text);
+}
+</style>
---
+++ src/components/Game.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="Game">
+    <div class="Game__Wrapper"></div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style></style>
